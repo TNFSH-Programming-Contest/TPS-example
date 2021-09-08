9f982411d4efe33202984dafb92f34809fe2b6bd
xiplus <huangxuanyuxiplus@gmail.com>
2021-09-08 13:27:10 +0800

    Remove unused check

--- pA/scripts/verify.py
+++ pA/scripts/verify.py
@@ -227,8 +227,8 @@ def verify_problem():
         except subprocess.CalledProcessError:
             warning('could not get git remote url for "{}"'.format(git_remote_name))
             return
-        if prob_name != git_main_remote_name:
-            warning('problem name and git project name are not the same')
+        # if prob_name != git_main_remote_name:
+        #     warning('problem name and git project name are not the same')
 
     check_problem_name(problem['name'])
 
